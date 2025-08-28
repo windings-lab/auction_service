6db7024df7c024a120a5198e0a0edc9a320db408
Gaster <janton20001@gmail.com>
2025-08-28 16:34:16 +0300

    Added health check if db connection exists

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
+import sys
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from .db import engine
-import src.model
 from src.auction.routers import router as auction_router
 from src.account.routers import router as account_router
 from src.etl.routers import router as etl_router
 
+async def test_db_connection():
+    try:
+        async with engine.connect():
+            print("Successfully connected to the database!")
+    except Exception as e:
+        print(f"Failed to connect to the database: {str(e)}. API won't work correctly.")
+        sys.exit(1)  # Exit with error code 1
 
 @asynccontextmanager
 async def lifespan(in_app: FastAPI):
+    await test_db_connection()
     yield
     # Shutdown code: nothing for now
     await engine.dispose()
