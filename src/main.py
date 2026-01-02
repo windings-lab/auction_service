b2f9cd37c39472fa173782a5832537bf8876c84e
Anton Zhukov <anton.zhukov@keymakr.com>
2026-01-03 00:48:59 +0200

    Deleted old ETL code

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ from fastapi import FastAPI
 from .db import engine
 from src.auction.routers import router as auction_router
 from src.account.routers import router as account_router
-from src.etl.routers import router as etl_router
 
 async def test_db_connection():
     try:
@@ -26,4 +25,3 @@ async def lifespan(in_app: FastAPI):
 fastapi_app = FastAPI(lifespan=lifespan)
 fastapi_app.include_router(auction_router)
 fastapi_app.include_router(account_router)
-fastapi_app.include_router(etl_router)
